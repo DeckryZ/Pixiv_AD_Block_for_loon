30fcfed451940d029cc6028bedf89e294d6259c2
Deckry <95526592+DeckryZ@users.noreply.github.com>
2025-12-10 00:18:39 +0800

    更新 pixiv_adblock.js

--- pixiv_adblock.js
+++ pixiv_adblock.js
@@ -8,7 +8,22 @@ if (url.indexOf("word=") !== -1 && body.illusts && Array.isArray(body.illusts)) 
     body.illusts.sort(function(a, b) {
         return (parseInt(b.total_bookmarks) || 0) - (parseInt(a.total_bookmarks) || 0);
     });
-    body.illusts = body.illusts.slice(0, 2);
+
+    var count = 2;
+    if (body.illusts.length > 2) {
+        var standard = parseInt(body.illusts[1].total_bookmarks) || 0;
+        var limit = standard * 0.7;
+        
+        for (var i = 2; i < 5 && i < body.illusts.length; i++) {
+            var current = parseInt(body.illusts[i].total_bookmarks) || 0;
+            if (current >= limit) {
+                count++;
+            } else {
+                break;
+            }
+        }
+    }
+    body.illusts = body.illusts.slice(0, count);
 }
 
 $done({ body: JSON.stringify(body) });
